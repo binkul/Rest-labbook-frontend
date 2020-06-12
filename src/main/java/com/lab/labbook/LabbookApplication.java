7fb11a9d23a4b7a23761d3c445c15161a9869274
binkul <jacekbinkul@gmail.com>
2020-06-12 10:48:55 +0200

    Initial commit

---
+++ src/main/java/com/lab/labbook/LabbookApplication.java
@@ -0,0 +1,13 @@
+package com.lab.labbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabbookApplication.class, args);
+    }
+
+}
